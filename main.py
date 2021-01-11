a9e9b3fac80d1841f2914d687e5d0b09f650cc96
kanisan007 <sabu01.kani@gmail.com>
2021-01-11 15:36:19 +0900

    dictにしたよ

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 
-#説明文
+# 説明文
 
 print('久遠に臥したるもの　死することなく　怪異なる永劫のうちには　死すら終焉を迎えん')
 
@@ -12,69 +12,65 @@ print('また、技能成功判定を行う場合は「３」を入力してく�
 
 
 ############################
-#やること決定
+# やること決定
 ############################
 
 yarukoto = int(input('なにをしますか？'))
 
 
 ############################
-#処理部分
+# 処理部分
 ############################
 
-if yarukoto==1:
-#ダイス機能   
+# ダイス機能
+if yarukoto == 1:
     dice = int(input('何面ダイス？'))
-
-    kekka = random.randint(1,dice)
-
+    kekka = random.randint(1, dice)
     print(kekka)
-elif yarukoto==2:
-#狂気表の参照機能
+
+# 狂気表の参照機能
+elif yarukoto == 2:
     list_dottika = int(input('リアルタイムなら「１」を、サマリーなら「２」を入力してください'))
+    dice_kekka = random.randint(1, 10)
 
+    # 新クトゥルフ神話TRPG ルールブック P418
+    madness_dict = {
+        'realtime': ['健忘症', '身体症状症', '暴力衝動', '偏執症', '重要な人々', '失神', 'パニックになって逃亡する', '身体的ヒステリーもしくは感情爆発', '恐怖症', 'マニア'],
+        'summary': ['健忘症', '盗難', '暴行', '暴力', 'イデオロギー/信念', '重要な人々', '収容', 'パニック', '恐怖症', 'マニア'],
+    }
 
-    dice_kekka = random.randint(1,10)
-    
-    listA = ['健忘症','身体症状症','暴力衝動','偏執症','重要な人々','失神','パニックになって逃亡する','身体的ヒステリーもしくは感情爆発','恐怖症','マニア']
-    listB = ['健忘症','盗難','暴行','暴力','イデオロギー/信念','重要な人々','収容','パニック','恐怖症','マニア']
+    # どってぃか
+    if list_dottika == 1:
+        print(madness_dict['realtime'][dice_kekka])
 
-    if list_dottika==1:
-        
-        print(listA[dice_kekka])
-
-    elif list_dottika==2:
-
-        print(listB[dice_kekka])
+    elif list_dottika == 2:
+        print(madness_dict['summary'][dice_kekka])
 
     else:
         print('入力が不正です。もう一度やり直してください。')
 
-elif yarukoto==3:
-#技能判定機能
-    ginou = input('技能の数値を入力してください')
+# 技能判定機
+elif yarukoto == 3:
+    ginou = int(input('技能の数値を入力してください'))
 
-    cc = random.randint(1,100)
-
-    ginou = int(ginou)
+    cc = random.randint(1, 100)
 
     print(f'目標技能値は{ginou}です。')
     print(f'1d100!結果は{cc}です。')
 
-
-    if ginou<cc:
-        if ginou<90:
+    if ginou < cc:
+        if ginou < 90:
             print('命中したが技能は失敗した。')
 
-        elif cc==100:
+        elif cc == 100:
             print('ファンブルです。どんまい！')
 
         else:
             print('攻撃は命中しなかった。')
 
-    elif ginou>=cc:
-        if ginou/2>=cc:
-            if ginou/5>=cc:
+    elif ginou >= cc:
+        if ginou / 2 >= cc:
+            if ginou / 5 >= cc:
                 print('エクストリーム成功です。')
             else:
                 print('ハード成功です')
@@ -86,7 +82,4 @@ elif yarukoto==3:
         print('エラーなのでもう一度やり直してください')
 
 else:
-        print('入力が不正です。もう一度やり直してください。')
-
-
-
+    print('入力が不正です。もう一度やり直してください。')
